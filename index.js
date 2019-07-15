9a846cd75ed288793238092ae41fb5551e26e407
Hamza  Javed <hamzajaved71@gmail.com>
2019-07-15 20:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solutions in this file
 function writeCards(arr){
+  a = [];
   for (let i=0; i <= (arr.length)-1 ; i++ ){
-    a = [];
+    
   a.push( (`Thank you, ${arr[i]}, for the wonderful surprise gift!`));
   }
   return a;
